16f715b38d5896d2fe85733e30133d46a71ec51f
Blake Scherschel <blake.scherschel@gmail.com>
2024-01-01 02:15:05 -0500

    Switch to sleep(500) instead of waiting for key. Formatting

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,34 @@
 #include "Examples.h"
 
 constexpr Index_t WIDTH = 100;
-constexpr Index_t HEIGHT = 50;
+constexpr Index_t HEIGHT = 30;
 
-void sleep(int millis)
-{
+void sleep(int millis) {
 	std::this_thread::sleep_for(std::chrono::milliseconds(millis));
 }
 
-int main()
-{
+int main() {
 	GameOfLife<WIDTH, HEIGHT> life;
 	ConsoleGridWriter<WIDTH, HEIGHT> grid;
 
 	life.setFromObject(2, 2, ex::spaceship_light);
 
-	do {
+	while (true) {
 		// Tick
 		life.tick();
 
 		// Update grid
-		for (Index_t x = 0; x < WIDTH; x++)
-			for (Index_t y = 0; y < HEIGHT; y++)
+		for (Index_t x = 0; x < WIDTH; x++) {
+			for (Index_t y = 0; y < HEIGHT; y++) {
 				grid.set(x, y, life.getState(x, y));
+			}
+		}
 
 		// Render grid
 		grid.render();
 
-	} while (getchar() == '\n');
+		sleep(500);
+	}
 
 	return 0;
 }
